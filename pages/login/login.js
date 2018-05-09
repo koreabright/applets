1f38826d94a62f44e2f8bcb1c7c74b349fccf485
hanshuang <hanshuang@yunniao.me>
2018-05-09 18:23:29 +0800

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,9 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    switchEye: true,
+    isDisabled: true,
+    params: {
+      username: '',
+      password: ''
+    }
   },
 
+  inputContent (e) {
+    if (e.target.id == 'username') {
+      this.setData({
+        'params.username': e.detail.value
+      })
+    }
+    if (e.target.id == 'password') {
+      this.setData({
+        'params.password': e.detail.value
+      })
+    }
+
+    if (this.data.params.username && this.data.params.password) {
+      this.setData({
+        isDisabled: false
+      });
+    }
+  },
+
+  checkData (params) {
+    let reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    console.log(reg);
+    console.log(params.username);
+    // if (!params.username.test(reg)) {
+    //   wx.showToast({
+    //     title: '邮箱格式不正确',
+    //     icon: 'none',
+    //     duration: 2000
+    //   });
+    // }
+  },
+
+  doLogin () {
+    this.checkData(this.data.params);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
