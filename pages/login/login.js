480cca7af4ba1fe1fb403c3eb6093db1f5b087fa
unknown <1403145614@qq.com>
2018-05-10 00:26:34 +0800

    tab

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,7 +1,6 @@
 // pages/login/login.js
 Page({
-
-  /**
+   /**
    * 页面的初始数据
    */
   data: {
@@ -13,16 +12,24 @@ Page({
     }
   },
 
+  onLoad () {
+    wx.setNavigationBarTitle({
+      title: '登录'
+    })
+  },
+  /**
+   * 输入用户名和密码
+   */
   inputContent (e) {
     if (e.target.id == 'username') {
       this.setData({
         'params.username': e.detail.value
-      })
+      });
     }
     if (e.target.id == 'password') {
       this.setData({
         'params.password': e.detail.value
-      })
+      });
     }
 
     if (this.data.params.username && this.data.params.password) {
@@ -31,76 +38,57 @@ Page({
       });
     }
   },
-
+   /**
+   * 切换明文显示密码
+   */
+  switchEye (e) {
+    this.setData({
+      switchEye: !this.data.switchEye
+    });
+  },
+   /**
+   * 登录时校验数据格式
+   */
   checkData (params) {
     let reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-    console.log(reg);
-    console.log(params.username);
-    // if (!params.username.test(reg)) {
-    //   wx.showToast({
-    //     title: '邮箱格式不正确',
-    //     icon: 'none',
-    //     duration: 2000
-    //   });
-    // }
+    if (params.username.length < 10) {
+      wx.showToast({
+        title: '邮箱长度大于10',
+        icon: 'none',
+        duration: 2000
+      });
+      return false;
+    }
+    if (params.password.length < 6) {
+      wx.showToast({
+        title: '密码长度大于6',
+        icon: 'none',
+        duration: 2000
+      });
+      return false;
+    }
+    return true;
   },
-
+   /**
+   * 登录
+   */
   doLogin () {
-    this.checkData(this.data.params);
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    let check = this.checkData(this.data.params);
+    if (!check) {
+      return;
+    }
+    wx.redirectTo({
+      url: '/pages/carteam/list'
+    })
+    // wx.request({
+    //   url: '/api/test',
+    //   data: this.data.params,
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data)
+    //   }
+    // });
   }
 })
